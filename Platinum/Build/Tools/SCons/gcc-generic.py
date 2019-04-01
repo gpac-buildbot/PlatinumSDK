cb166d3164764d1f8728733ad17e21e7102d5a8a
Aurelien David <aurelien.david@telecom-paristech.fr>
2019-04-01 12:00:01 +0200

    force c++98 to build on newer platforms

--- Platinum/Build/Tools/SCons/gcc-generic.py
+++ Platinum/Build/Tools/SCons/gcc-generic.py
@@ -2,26 +2,28 @@ import os
 
 def generate(env, gcc_cross_prefix=None, gcc_strict=True, gcc_stop_on_warning=None, gcc_extra_options=''):
     if gcc_stop_on_warning == None: gcc_stop_on_warning = env['stop_on_warning']
- 
+
     ### compiler flags
     if gcc_strict:
         env.AppendUnique(CCFLAGS = ['-pedantic', '-Wall',  '-W',  '-Wundef', '-Wno-long-long', '-fPIC'])
         env.AppendUnique(CFLAGS  = ['-Wmissing-prototypes', '-Wmissing-declarations'])
     else:
         env.AppendUnique(CCFLAGS = ['-Wall', '-fPIC'])
-    
+
     compiler_defines = ['-D_REENTRANT']
     env.AppendUnique(CCFLAGS  = compiler_defines)
     env.AppendUnique(CPPFLAGS = compiler_defines)
-    
+
     if env['build_config'] == 'Debug':
         env.AppendUnique(CCFLAGS = '-g')
     else:
         env.AppendUnique(CCFLAGS = '-O3')
-    
+
+    env.AppendUnique(CCFLAGS = '-std=c++98')
+
     if gcc_stop_on_warning:
         env.AppendUnique(CCFLAGS = ['-Werror'])
-        
+
     env['STRIP']  = 'strip'
 
     if gcc_cross_prefix:
@@ -33,4 +35,3 @@ def generate(env, gcc_cross_prefix=None, gcc_strict=True, gcc_stop_on_warning=No
         env['LINK']   = gcc_cross_prefix+'-g++ ' + gcc_extra_options
         env['STRIP']  = gcc_cross_prefix+'-strip'
 
-    
