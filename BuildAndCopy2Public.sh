7c439663a53bcd70e4f331997946d7f604b52572
Aurelien David <aurelien.david@telecom-paristech.fr>
2019-04-02 15:49:34 +0200

    fix build on osx

--- BuildAndCopy2Public.sh
+++ BuildAndCopy2Public.sh
@@ -3,13 +3,15 @@ if [ $# -ne 1 ] ; then
   echo "Usage: `basename $0` scons_platform"
   exit 1
 fi
-
+set +e
 cd Platinum
 scons -j 4
+ret=$?
 # some might fail but the useful .a still be there, so don't exit if errors
 # if [ $? != 0 ] ; then
 # 	exit 1
 # fi
 mkdir -p ../../../gpac_public/extra_lib/lib/gcc
-cp Build/Targets/$1/Debug/*.a ../../../gpac_public/extra_lib/lib/gcc
+cp -av Build/Targets/$1/Debug/*.a ../../../gpac_public/extra_lib/lib/gcc
 
+exit $ret
